fd758ebe6fb386b09c760ffbc430d6eee8bfe0cb
wahyuiqbal <wahyuiqbal91@gmail.com>
2022-03-15 21:14:06 +0700

    => Add contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Wahyu Iqbal Saya tinggal di Jakarta Utara, Saya suka bermain game";
+	}
+}
